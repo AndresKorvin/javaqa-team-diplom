9f75c46305644b4de642e296dbe8f2ff9ca21288
AndresKorvin <andreskorvin@gmail.com>
2023-03-04 20:21:59 +0300

    Stage 3: поиск девектов, написаие тестов, составление баг-репортов

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -1,29 +1,32 @@
 package ru.netology;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameStoreTest {
 
-    // Тест сразу двух методов store.publishGame и store.containsGame
-    // Так как поле List<Game> games класса GameStore приватное, метод publishGame не проверить без метода containsGame
+    @Test
+    public void shouldAddGame() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        assertTrue(store.containsGame(game));
+    }
+
     @ParameterizedTest
     @CsvSource({
             "Дюна, Стратегия, Дум, Шутер, NFS, Гонки", //Все игры с разными названиями и жанрами
             "Дюна, Стратегия, Дум, Шутер, Дюна, Гонки", //Игра №3 с повторяющимся называнием
-            "Дюна, Стратегия, Дум, Шутер, NFS, Стратегия" //Игра №3 с повторяющимся жанром
+            "Дюна, Стратегия, Дум, Шутер, NFS, Стратегия", //Игра №3 с повторяющимся жанром
+            "Дюна, Стратегия, Дум, Шутер, Дюна, Стратегия" //Игра №3 с повторяющимися и названием, и жанром
     })
-    public void shouldAddGame(String name1,String genre1,String name2,String genre2,String name3,String genre3) {
+    public void shouldContainsGame(String name1, String genre1, String name2, String genre2, String name3, String genre3) {
+        // Проверка двух методов store.publishGame и store.containsGame, так как один без другого проверить невозможно, из-за того что поля приватные
+        // Баг - Метод containsGame класса GameStore не проверяет наличие Последнего элемента в списке List<Game> games
 
         GameStore store = new GameStore();
         Game game1 = store.publishGame(name1, genre1);
@@ -35,44 +38,95 @@ public class GameStoreTest {
         assertTrue(store.containsGame(game3));
     }
 
-    // Тест сразу двух методов store.publishGame и store.containsGame
-    // Так как поле List<Game> games класса GameStore приватное, метод publishGame не проверить без метода containsGame
-    @ParameterizedTest
-    @CsvSource({
-            "Дюна, Стратегия, Дум, Шутер, Дюна, Стратегия", //Игра №3 с повторяющимися и названием, и жанром
-    })
-    public void shouldNotAddGame(String name1,String genre1,String name2,String genre2,String name3,String genre3) {
+    @Test
+    public void shouldRememberDirectoryObject() {
 
         GameStore store = new GameStore();
-        Game game1 = store.publishGame(name1, genre1);
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        GameStore expected = store;
+        GameStore actual = game.getStore();
+
+        assertEquals(expected, actual);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "Дюна, Стратегия, Дум, Шутер, NFS, Гонки", //Все игры с разными названиями и жанрами
+            "Дюна, Стратегия, Дум, Шутер, Дюна, Гонки", //Игра №3 с повторяющимся называнием
+            "Дюна, Стратегия, Дум, Шутер, NFS, Стратегия", //Игра №3 с повторяющимся жанром
+    })
+    public void shouldNotContainsGame(String name1, String genre1, String name2, String genre2, String name3, String genre3) {
+
+        GameStore storeTest = new GameStore();
+        Game game1 = storeTest.publishGame(name1, genre1);
+
+        GameStore store = new GameStore();
         Game game2 = store.publishGame(name2, genre2);
         Game game3 = store.publishGame(name3, genre3);
 
-        assertTrue(store.containsGame(game1));
-        assertTrue(store.containsGame(game2));
+        assertFalse(storeTest.containsGame(game2));
+        assertFalse(storeTest.containsGame(game3));
+        assertFalse(store.containsGame(game1));
 
-        assertFalse(store.containsGame(game3)); // Не содержит game3
     }
 
-    /**
-     * Регистрирует количество времени, которое проиграл игрок
-     * за игрой этого каталога. Игрок задаётся по имени. Время должно
-     * суммироваться с прошлым значением для этого игрока
-     */
     @ParameterizedTest
     @CsvSource({
-            "Андрей, 1, Игорь, 3, Роман, 5,     1, 3, 5", //Игра №3 с повторяющимися и названием, и жанром
+            "Андрей, 4, Игорь, 3, Роман, 5", // 3 разных игрока с одной игрой
+            "Игорь, 0, Роман, 0, Андрей, 1", // 3 игрока, максимальное время - 1 час
+            "Андрей, 7, Игорь, 3, Игорь, 6", // 2 игрока, один из них играл несколько раз
+            "Игорь, 0, Игорь, 0, Андрей, 100" // 2 игрока, один из них играл два раза по 0 часов
 
     })
-    public void shouldAddPlayTime(String name1, int hours1, String name2, int hours2, String name3, int hours3,
-                                  int resultHours1, int resultHours2, int resultHours3) {
+    public void shouldAddPlayTime(String name1, int hours1, String name2, int hours2, String name3, int hours3) {
+        // Проверка двух методов store.addPlayTime и store.getMostPlayer, так как один без другого проверить невозможно, из-за того что поля приватные
+        // Баг - Метод addPlayTime класса GameStore не суммируется с прошлым значением для этого игрока
+        // Баг - Метод addPlayTime класса GameStore не выводит время, если максимальное время - 1 час
+
         GameStore store = new GameStore();
 
         store.addPlayTime(name1, hours1);
         store.addPlayTime(name2, hours2);
         store.addPlayTime(name3, hours3);
 
+        String expected = name3;
+        String actual = store.getMostPlayer();
 
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void shouldNotGetMostPlayer() {
+
+        GameStore store = new GameStore();
+
+        String expected = null;
+        String actual = store.getMostPlayer();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Андрей, 4, Игорь, 3, Роман, 5", // 3 разных игрока с разным временем
+            "Андрей, 7, Игорь, 3, Игорь, 6", // 2 игрока, один из них играл несколько раз
+            "Игорь, 0, Игорь, 0, Андрей, 1" // 2 игрока, у одного два раза по 0 часов
+
+    })
+    public void shouldGetSumPlayedTime(String name1, int hours1, String name2, int hours2, String name3, int hours3) {
+        // Баг - Метод getSumPlayedTime класса GameStore не суммирует общее количество времени всех игроков, проведённого за играми этого каталога
+
+        GameStore store = new GameStore();
+
+        store.addPlayTime(name1, hours1);
+        store.addPlayTime(name2, hours2);
+        store.addPlayTime(name3, hours3);
+
+        int expected = hours1 + hours2 + hours3;
+        int actual = store.getSumPlayedTime();
+
+        assertEquals(expected, actual);
     }
 }
